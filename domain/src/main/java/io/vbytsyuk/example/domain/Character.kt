89c7aeb73c550b35a7e3deb6a239f1f486c60fad
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2021-07-12 19:45:41 +0300

    Added `:domain` module

---
+++ domain/src/main/java/io/vbytsyuk/example/domain/Character.kt
@@ -0,0 +1,11 @@
+package io.vbytsyuk.example.domain
+
+data class Character(
+    val id: Int,
+    val name: String,
+    val species: String,
+    val status: String,
+    val gender: String,
+    val imageUrl: String,
+    val locationName: String,
+)
---
+++ domain/src/main/java/io/vbytsyuk/example/domain/Episode.kt
@@ -0,0 +1,8 @@
+package io.vbytsyuk.example.domain
+
+data class Episode(
+    val id: Int,
+    val name: String,
+    val airDate: String,
+    val episodeCode: String,
+)
